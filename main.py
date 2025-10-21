a862a69df44a2ae8e2f479e494d97d92a160cf82
mookoo15907 <z4r7sfxf9z@privaterelay.appleid.com>
2025-10-21 19:51:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 import random  # 新增：用于随机选择回复
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
+@register("astrbot_plugin_ceshi", "mookoo", "", "1.0.0")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
